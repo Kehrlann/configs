03f3fced1591c9420d39425fb4d6f9bb8a8b8531
Daniel Garnier-Moiroux <git@garnier.wf>
2019-08-20 14:10:17 +0200

    Move nvm.bash to bash_it plugins from custom

--- bash-it/plugins/available/nvm.plugin.bash
+++ bash-it/plugins/available/nvm.plugin.bash
@@ -1,3 +1,3 @@
 export nvm_dir="$HOME/.nvm"
-[ -s "$nvm_dir/nvm.sh" ] && \. "$nvm_dir/nvm.sh"  # this loads nvm
+[ -s "$nvm_dir/nvm.sh" ] && source "$nvm_dir/nvm.sh" --no-use # this loads nvm
 [[ -r $NVM_DIR/bash_completion ]] && \. $NVM_DIR/bash_completion
