5da612cd79f39d604295f6643be8b81e354340f3
Daniel Garnier-Moiroux <git@garnier.wf>
2020-02-28 16:49:58 +0100

    Add git duet initials to zsh

--- oh-my-zsh/custom/powerlevel9k.zsh
+++ oh-my-zsh/custom/powerlevel9k.zsh
@@ -1,4 +1,11 @@
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time dir vcs)
+git_duet_initials(){
+    GIT_DUET_INITIALS=$(echo $(git config --get-regexp "^duet.env.git-.*-initials" 2>/dev/null | sed -e 's/^.*-initials //') | sed -e 's/ /+/')
+    echo -n " $GIT_DUET_INITIALS %{%f%}"
+}
+
+POWERLEVEL9K_CUSTOM_GIT_DUET="git_duet_initials"
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time dir custom_git_duet vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
 
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
