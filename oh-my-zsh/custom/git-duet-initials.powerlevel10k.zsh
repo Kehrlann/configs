18b2892ed586cc25c9dec648e08847ebe5f24e1c
Daniel Garnier-Moiroux <git@garnier.wf>
2020-07-08 14:09:23 +0200

    Add git duet initials for powerlevel10k

---
+++ oh-my-zsh/custom/git-duet-initials.powerlevel10k.zsh
@@ -0,0 +1,9 @@
+################################################################
+# Git-duet: Support for committer/author pair in git
+# see: https://github.com/git-duet/git-duet
+function prompt_git_duet() {
+  GIT_DUET_INITIALS=$(echo $(git config --get-regexp "^duet.env.git-.*-initials" 2>/dev/null | sed -e 's/^.*-initials //') | sed -e 's/ /+/')
+  if [[ ! -z "$GIT_DUET_INITIALS" ]]; then
+    p10k segment -f 15 -t "$GIT_DUET_INITIALS"
+  fi
+}
