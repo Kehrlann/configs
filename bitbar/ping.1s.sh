15ba3ba714ceb5f14d48313f35f9bf75c9408730
Daniel Garnier-Moiroux <git@garnier.wf>
2020-07-23 17:42:57 +0200

    Add bit-bar plugin for ping

---
+++ bitbar/ping.1s.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+STATE_FILE="$SCRIPT_DIR/ping-state.txt"
+GREEN=🟩
+YELLOW=🟨
+RED=🟥
+
+# touch the statefile ; in case it doesn't exist
+touch "$STATE_FILE"
+
+# Ping google
+PING_RESULT=$(ping -c 1 -n -q google.com 2>/dev/null)
+PING=1000
+if [[ ! -z $PING_RESULT ]]; then
+  PING=$(echo "$PING_RESULT" | awk -F '/' 'END {printf "%.0f\n", $5}')
+fi
+
+# Load previous state
+PREVIOUS_STATE=$(cat $STATE_FILE)
+if [[ ${#PREVIOUS_STATE} -gt 12 ]]; then
+  PREVIOUS_STATE=${PREVIOUS_STATE:4}
+fi
+
+# Compute new state
+NEW_STATE=$GREEN
+if [[ $PING -ge 1000 ]]; then
+  NEW_STATE=$RED
+elif [[ $PING -ge 100 ]]; then
+  NEW_STATE=$YELLOW
+fi
+NEW_STATE=$PREVIOUS_STATE$NEW_STATE
+
+# Output
+echo $NEW_STATE | tee $STATE_FILE
