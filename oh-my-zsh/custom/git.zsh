1bd9e088c2c15800ee9aac6167815357bb54ac37
Daniel Garnier-Moiroux <git@garnier.wf>
2021-02-04 12:26:04 +0100

    Move git config; default branch name becoms "main"

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,8 +1,9 @@
+# Git related stuff
+alias gitc='git duet-commit -m'
+
 export GIT_DUET_ROTATE_AUTHOR=1
 
-################################################################
-# Git-duet: Support for committer/author pair in git
-# see: https://github.com/git-duet/git-duet
+## zsh prompt duet initials
 function prompt_git_duet() {
   GIT_DUET_INITIALS=$(echo $(git config --get-regexp "^duet.env.git-.*-initials" 2>/dev/null | sed -e 's/^.*-initials //') | sed -e 's/ /+/')
   if [[ ! -z "$GIT_DUET_INITIALS" ]]; then
