8dd1005e0bf6eeb3045cf4f98906f982f0eaca59
Daniel Garnier-Moiroux <git@garnier.wf>
2022-11-03 21:55:19 +0100

    update photo transform script

--- bash-snippets/get_exif.sh
+++ bash-snippets/get_exif.sh
@@ -3,12 +3,20 @@
 set -eou
 
 function date_exif() {
-  mdls $1 | grep "kMDItemContentCreationDate" | head -n1 | sed 's/.*= //g' | cut -c1-10
+  mdls $1 | grep "kMDItemContentCreationDate" | head -n1 | sed 's/.*= //g' | gdate +"%Y_%m_%d" -f -
 }
 
 mdls -h
+magick mogrify -monitor -format jpg *.heic || true
+rm *.heic || true
 
-for f in DSC*; do
-  photo_date=$(date_exif $f | sed 's/\-//g')
-  cp $f IMG-${photo_date}_dsc.jpg
+mkdir output
+
+for f in *.jpg; do
+  echo "Transforming $f"
+  photo_date="$(date_exif $f)-$(uuidgen | cut -c1-8)"
+  echo "Into         output/IMG-${photo_date}.jpg"
+  cp "$f" "output/IMG-${photo_date}.jpg"
 done
+
+zip -r output.zip output
