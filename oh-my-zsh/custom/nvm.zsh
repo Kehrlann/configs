10a6798e80dc7d7c75289575840d922689d81a41
Daniel Garnier-Moiroux <git@garnier.wf>
2023-02-07 22:18:30 +0100

    oh-my-zsh custom lazy-load nvm

---
+++ oh-my-zsh/custom/nvm.zsh
@@ -0,0 +1,26 @@
+_lazy_load_nvm() {
+  unset -f node nvm npm npx
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+}
+
+node() {
+  _lazy_load_nvm
+  node $@
+}
+
+nvm() {
+  _lazy_load_nvm
+  nvm $@
+}
+
+npm() {
+  _lazy_load_nvm
+  npm $@
+}
+
+npx() {
+  _lazy_load_nvm
+  npx $@
+}
