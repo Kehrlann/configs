c00d1778cf1f3701e4c21e8b4d5be0817e99f5ab
Daniel Garnier-Moiroux <git@garnier.wf>
2023-08-07 17:06:14 +0200

    minor updates

--- oh-my-zsh/custom/m1/path.zsh
+++ oh-my-zsh/custom/m1/path.zsh
@@ -1,5 +1,6 @@
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/workspace/go/bin:$PATH"
 export PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
-export PATH="$GEM_HOME/bin:$PATH"
+export PATH="/opt/homebrew/Cellar/bash/5.2.15/bin:$PATH"
 export PATH="/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin:$PATH"
+export PATH="/Library/Ruby/Gems/2.6.0/gems/cf-uaac-4.14.0/bin:$PATH"
