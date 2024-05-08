0f960446555119136288a976369d23c761b424db
Daniel Garnier-Moiroux <git@garnier.wf>
2024-05-08 14:57:47 +0200

    update m1 path config

--- oh-my-zsh/custom/m1/path.zsh
+++ oh-my-zsh/custom/m1/path.zsh
@@ -4,4 +4,6 @@ export PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
 export PATH="/opt/homebrew/Cellar/bash/5.2.15/bin:$PATH"
 export PATH="/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin:$PATH"
 export PATH="/Library/Ruby/Gems/2.6.0/gems/cf-uaac-4.14.0/bin:$PATH"
+export PATH="/Users/dgarnier/.local/bin:$PATH"
 export PATH=".:$PATH"
+
