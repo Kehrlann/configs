7ff397f5ba50cb0160e9b5685aba717207f65388
Daniel Garnier-Moiroux <git@garnier.wf>
2025-03-07 11:40:21 +0100

    bash-snippet: get_exif

---
+++ bash-snippets/get_exif.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eou
+
+function date_exif() {
+  create_date=$(identify -format $1)
+  exif_date=$(identify -verbose $1)
+  if 
+
+  mdls $1 | grep "kMDItemContentCreationDate" | head -n1 | sed 's/.*= //g' | gdate +"%Y_%m_%d" -f -
+}
+
+mogrify -monitor -format jpg *.heic *.HEIC || true
+rm *.heic *.HEIC || true
+
+mkdir output
+
+for f in *.jpg; do
+  echo "Transforming $f"
+  photo_date="$(date_exif $f)-$(uuidgen | cut -c1-8)"
+  echo "Into         output/IMG-${photo_date}.jpg"
+  cp "$f" "output/IMG-${photo_date}.jpg"
+done
+
+zip -r output.zip output
